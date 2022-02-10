514ead53f68b8c166e93e88e1981758c44aa40a2
Lunfer <zozoukazizizoi@hotmail.com>
2022-02-10 17:59:22 +0200

    changes to the counter application up until course number 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React, { useState } from "react";
 import Counter from "./counter";
 import CounterHooks from "./counterHooks";
 
+export const ThemeContext = React.createContext();
+
 function App() {
+  const [theme, setTheme] = useState("red");
   return (
-    <div>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
       Counter
       <Counter initialCount={0} />
       CounterHooks
       <CounterHooks initialCount={0} />
-    </div>
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === "red" ? "blue" : "red";
+          })
+        }
+      >
+        Toggle Theme
+      </button>
+    </ThemeContext.Provider>
   );
 }
 
